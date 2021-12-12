eb288d48fd23e2f4ba48ed648ea041b01bd04c7f
Pechenyice <german.iskhakov@mail.ru>
2021-12-12 22:21:16 +0300

    updated config to comfortable dev and prod deploy

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
-module.exports = {
+let config = {
   reactStrictMode: true,
-}
+};
+
+const isProd = process.env.NODE_ENV.toLowerCase() === "production";
+
+if (isProd)
+  config = {
+    ...config,
+    images: {
+      loader: "imgix",
+      path: "https://Pechenyice.github.io/",
+    },
+    basePath: "/deployNextToGHPages",
+    assetPrefix: "/deployNextToGHPages",
+  };
+
+module.exports = config;
