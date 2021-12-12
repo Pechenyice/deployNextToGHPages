fbe2ac9c15201e4c2d10ee95a1315ed998a18532
Pechenyice <german.iskhakov@mail.ru>
2021-12-12 20:58:53 +0300

    initial commit with default routing

---
+++ pages/manual.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Manual() {
+  return (
+    <div className={styles.container}>
+      <p>Read full manual on README.MD in github repository!</p>
+    </div>
+  );
+}
